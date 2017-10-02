03cf2210b8a7f1551c02135e363005624279edee
lautarofernandez27 <lautaro.fernandez27@gmail.com>
2017-10-02 11:06:10 -0300

    jjj

---
+++ src/javaapplication9/JavaApplication9.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication9;
+
+import Vista.NewJFrame;
+
+/**
+ *
+ * @author lauta
+ */
+public class JavaApplication9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJFrame p = new NewJFrame();
+        p.setVisible(true);
+    // TODO code application logic here
+    }
+    
+}
